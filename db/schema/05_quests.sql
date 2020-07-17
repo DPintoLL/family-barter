8bba4cf3ba7195f2ed0dd0524d96fb2f146c6341
Sean Sullivan <dev@seanmedia.ca>
2020-07-16 21:18:04 -0600

    Add schema for quests table

---
+++ db/schema/05_quests.sql
@@ -0,0 +1,19 @@
+-- Drop and recreate quests table
+
+DROP TABLE IF EXISTS quests;
+
+CREATE TABLE quests (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  family_id INTEGER REFERENCES families(id) NOT NULL,
+  title VARCHAR(255),
+  description VARCHAR(255),
+  reward INTEGER NOT NULL,
+  parent_id INTEGER REFERENCES quest(id),
+  index_number INTEGER DEFAULT 1,
+  assigned_to INTEGER REFERENCES users(id),
+  is_completed BOOLEAN NOT NULL DEFAULT 0,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  created_by INTEGER REFERENCES users(id) NOT NULL,
+  updated_at TIMESTAMP,
+  updated_by INTEGER REFERENCES users(id)
+);
