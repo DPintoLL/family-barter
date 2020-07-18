d7df93badcd314338daa00fa98eff20e4d55927d
kevinconvery <kevinconvery@gmail.com>
2020-07-18 17:04:51 -0400

    add families table seed

---
+++ db/seeds/01_families.sql
@@ -0,0 +1,5 @@
+INSERT INTO families (name) VALUES ("King");
+INSERT INTO families (name) VALUES ("Koelpin");
+INSERT INTO families (name) VALUES ("Hammes");
+INSERT INTO families (name) VALUES ("Reinger");
+INSERT INTO families (name) VALUES ("Cummings");
