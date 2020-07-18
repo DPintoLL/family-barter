0002bb2db159f87f3fff0044f8b4478b945906bf
Sean Sullivan <dev@seanmedia.ca>
2020-07-18 12:54:20 -0600

    Restricted prop types for Quest component

--- src/components/Quest/index.tsx
+++ src/components/Quest/index.tsx
@@ -1,40 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import "./styles.scss";
 
-const COMMON = "COMMON";
-const UNCOMMON = "UNCOMMON";
-const RARE = "RARE";
-const LEGENDARY = "LEGENDARY";
-const EPIC = "EPIC";
+type DifficultyTypes = "common" | "uncommon" | "rare" | "legendary" | "epic";
 
 interface QuestProps {
-  length: number;
+  difficulty: DifficultyTypes;
 }
 
 export default function Quest(props: QuestProps) {
-  const [difficulty, setDifficulty] = useState(UNCOMMON);
-
-  useEffect(() => {
-    if (props.length === 1) {
-      setDifficulty(COMMON);
-    }
-    if (props.length === 2) {
-      setDifficulty(UNCOMMON);
-    }
-    if (props.length === 3) {
-      setDifficulty(RARE);
-    }
-    if (props.length === 4) {
-      setDifficulty(LEGENDARY);
-    }
-    if (props.length === 5) {
-      setDifficulty(EPIC);
-    }
-  }, [props.length, setDifficulty]);
-
+  const { difficulty } = props;
   return (
-    <div className={"quest " + difficulty.toLowerCase()}>
+    <div className={["quest", difficulty].join(" ")}>
       <article>A Quest!</article>
     </div>
   );
--- src/components/__stories__/Quest.stories.js
+++ src/components/__stories__/Quest.stories.js
@@ -4,8 +4,8 @@ import { storiesOf } from "@storybook/react";
 import Quest from "components/Quest";
 
 storiesOf("Quest", module)
-  .add("Common Quest", () => <Quest length={1} />)
-  .add("Uncommon Quest", () => <Quest length={2} />)
-  .add("Rare Quest", () => <Quest length={3} />)
-  .add("Legendary Quest", () => <Quest length={4} />)
-  .add("Epic Quest", () => <Quest length={5} />);
+  .add("Common Quest", () => <Quest difficulty={"common"} />)
+  .add("Uncommon Quest", () => <Quest difficulty={"uncommon"} />)
+  .add("Rare Quest", () => <Quest difficulty={"rare"} />)
+  .add("Legendary Quest", () => <Quest difficulty={"legendary"} />)
+  .add("Epic Quest", () => <Quest difficulty={"epic"} />);
