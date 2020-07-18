78cd702adf576a15a91c68f3427e7bfb44e74a0a
kevinconvery <kevinconvery@gmail.com>
2020-07-18 17:06:03 -0400

    add users table seed

---
+++ db/seeds/03_users.sql
@@ -0,0 +1,55 @@
+INSERT into users (
+  family_id, 
+  first_name, 
+  last_name,
+  email,
+  password_hash,
+  type_id,
+  invited_by
+) VALUES (
+  -- family of 5, the King family
+  -- Devin, Ayana the parent accounts
+  -- Devin the admin account
+  -- Brent/Lonny/Griffin child accounts
+  1,
+  "Devin",
+  "King",
+  "devin@king.com",
+  "PASSWORD_HASH_HERE",
+  1,
+  null
+),
+(
+  1,
+  "Ayana",
+  "King",
+  "ayana@king.com",
+  "PASSWORD_HASH_HERE",
+  2,
+  1
+),
+(
+  1,
+  "Brent",
+  "King",
+  "brent@king.com",
+  "PASSWORD_HASH_HERE",
+  3,
+  1
+),
+(
+  "Lonny",
+  "King",
+  "lonny@king.com",
+  "PASSWORD_HASH_HERE",
+  3,
+  1
+),
+(
+  "Griffin",
+  "King",
+  "griffin@king.com",
+  "PASSWORD_HASH_HERE",
+  3,
+  1
+);
