1a0174e79d71d75158f71b0f0587a7e05eba3e6b
kevinconvery <kevinconvery@gmail.com>
2020-07-19 17:21:40 -0400

    debug prizes seed

--- db/seeds/08_prizes.sql
+++ db/seeds/08_prizes.sql
@@ -11,12 +11,12 @@ INSERT INTO prizes (
   /* red sneakers */
   /* id: 1 */
   1,
-  "Red Sneakers"
-  "These are a pair of cool red sneakers!",
+  'Red Sneakers',
+  'These are a pair of cool red sneakers!',
   50,
   /* placeholders for icon and colour */
-  "ICON_URL_HERE",
-  "#FFFFFF",
+  'ICON_URL_HERE',
+  '#FFFFFF',
   true,
   1
 ),
@@ -24,11 +24,11 @@ INSERT INTO prizes (
   /* blue kite */
   /* id: 2 */
   1,
-  "Blue Kite",
-  "Let's all go fly a kite together!",
+  'Blue Kite',
+  'Let''s all go fly a kite together!',
   85,
-  "ICON_URL_HERE",
-  "#FFFFFF",
+  'ICON_URL_HERE',
+  '#FFFFFF',
   true,
   1
 ),
@@ -36,11 +36,11 @@ INSERT INTO prizes (
   /* Silly Mask */
   /* id: 3 */
   1,
-  "Silly Mask",
-  "This fun mask is perfect for costume parties!",
+  'Silly Mask',
+  'This fun mask is perfect for costume parties!',
   25,
-  "ICON_URL_HERE",
-  "#FFFFFF",
+  'ICON_URL_HERE',
+  '#FFFFFF',
   true,
   1
 ),
@@ -49,11 +49,11 @@ INSERT INTO prizes (
   /* Rabbit Ears */
   /* id: 4 */
   1,
-  "Rabbit Ears",
-  "Put these on for some added bunny style!",
+  'Rabbit Ears',
+  'Put these on for some added bunny style!',
   25,
-  "ICON_URL_HERE",
-  "#FFFFFF",
+  'ICON_URL_HERE',
+  '#FFFFFF',
   false,
   2
 ),
@@ -62,11 +62,11 @@ INSERT INTO prizes (
   /* White T-Shirt */
   /* id: 5 */
   1,
-  "White T-Shirt",
-  "A great shirt for wearing anywhere!",
+  'White T-Shirt',
+  'A great shirt for wearing anywhere!',
   70,
-  "ICON_URL_HERE",
-  "#FFFFFF",
+  'ICON_URL_HERE',
+  '#FFFFFF',
   false,
   1
-)
+);
