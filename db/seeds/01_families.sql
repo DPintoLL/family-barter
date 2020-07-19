55d931cc1ad78fac903d757f15c1b2343554768e
kevinconvery <kevinconvery@gmail.com>
2020-07-19 17:10:29 -0400

    refactor insert statement

--- db/seeds/01_families.sql
+++ db/seeds/01_families.sql
@@ -1,7 +1,9 @@
 -- families seed file
 -- seeded more families if we want to test adding users from that
-INSERT INTO families (surname) VALUES ('King');
-INSERT INTO families (surname) VALUES ('Koelpin');
-INSERT INTO families (surname) VALUES ('Hammes');
-INSERT INTO families (surname) VALUES ('Reinger');
-INSERT INTO families (surname) VALUES ('Cummings');
+INSERT INTO families (surname) VALUES 
+  ('King'),
+  ('Koelpin'),
+  ('Hammes'),
+  ('Reinger'),
+  ('Cummings');
+
