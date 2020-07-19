bd841797df5064781e4d4dafe48d39c4a59bc3c9
kevinconvery <kevinconvery@gmail.com>
2020-07-19 16:59:12 -0400

    debug families table seed

--- db/seeds/01_families.sql
+++ db/seeds/01_families.sql
@@ -1,7 +1,7 @@
 -- families seed file
 -- seeded more families if we want to test adding users from that
-INSERT INTO families (name) VALUES ("King");
-INSERT INTO families (name) VALUES ("Koelpin");
-INSERT INTO families (name) VALUES ("Hammes");
-INSERT INTO families (name) VALUES ("Reinger");
-INSERT INTO families (name) VALUES ("Cummings");
+INSERT INTO families (surname) VALUES ('King');
+INSERT INTO families (surname) VALUES ('Koelpin');
+INSERT INTO families (surname) VALUES ('Hammes');
+INSERT INTO families (surname) VALUES ('Reinger');
+INSERT INTO families (surname) VALUES ('Cummings');
