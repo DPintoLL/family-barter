9d705f543cc0436a8400eaa5dd807a132a78b475
Sean Sullivan <dev@seanmedia.ca>
2020-07-20 11:22:19 -0600

    Add styling to Quest component

--- src/components/__stories__/Quest.stories.js
+++ src/components/__stories__/Quest.stories.js
@@ -4,8 +4,16 @@ import { storiesOf } from "@storybook/react";
 import Quest from "components/Quest";
 
 storiesOf("Quest", module)
-  .add("Common Quest", () => <Quest difficulty={"common"} />)
-  .add("Uncommon Quest", () => <Quest difficulty={"uncommon"} />)
-  .add("Rare Quest", () => <Quest difficulty={"rare"} />)
-  .add("Legendary Quest", () => <Quest difficulty={"legendary"} />)
-  .add("Epic Quest", () => <Quest difficulty={"epic"} />);
+  .add("Common Quest", () => (
+    <Quest title={"A Common Quest"} difficulty={"common"} />
+  ))
+  .add("Uncommon Quest", () => (
+    <Quest title={"An Uncommon Quest"} difficulty={"uncommon"} />
+  ))
+  .add("Rare Quest", () => <Quest title={"A Rare Quest"} difficulty={"rare"} />)
+  .add("Legendary Quest", () => (
+    <Quest title={"A Legendary Quest"} difficulty={"legendary"} />
+  ))
+  .add("Epic Quest", () => (
+    <Quest title={"An Epic Quest"} difficulty={"epic"} />
+  ));
