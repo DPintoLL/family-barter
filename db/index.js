ec5d9076f922b1fb8bb00d2cf05cba59ed16a887
Sean Sullivan <dev@seanmedia.ca>
2020-07-20 11:25:57 -0600

    Add database connection class

--- db/index.js
+++ db/index.js
@@ -1,22 +1,64 @@
-const chalk = require("chalk");
-const pg = require("pg");
+// db/index.js
 
-const client = new pg.Client({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-});
+// load .env data into process.env
+require("dotenv").config();
 
-console.log(chalk.cyan(`-> Connecting to database ...`));
-client
-  .connect()
-  .then(() =>
-    console.log(chalk.green("Successfully connected to Postgres server."))
-  )
-  .catch((err) =>
-    console.error(chalk.red(`Error connecting to Postgres server:\n${err}`))
-  );
+const { Pool } = require("pg");
 
-module.exports = client;
+const FamiliesTable = require("./tables/families");
+
+/**
+ * Manages all interactions with the database.
+ */
+class DatabaseConnection {
+  constructor() {
+    this.pool = new Pool(this._parameters());
+
+    // Connected database tables
+    this.families = new FamiliesTable(this);
+  }
+
+  /**
+   * Sets database connection parameters from environmental variables.
+   */
+  _parameters() {
+    return {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      database: process.env.PGDATABASE,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      max: 3,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
+    };
+  }
+
+  /**
+   * Execute an SQL query against the connected database.
+   * @param {String} text - A valid SQL query string.
+   * @param {Object} params - Query parameters.
+   */
+  async query(text, params) {
+    const client = await this.pool.connect();
+    const response = await client.query(text, params);
+    client.release();
+    return response.rows;
+  }
+
+  /**
+   * Checkout a client from connection pool.
+   */
+  client() {
+    return this.pool.connect();
+  }
+}
+
+// Instantiate the database connection
+const databaseConnection = new DatabaseConnection();
+
+// Freeze the database connection object
+Object.freeze(databaseConnection);
+
+// Export the database connection
+module.exports = databaseConnection;
