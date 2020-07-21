cf2d19201827260a0aaa77316417601abc066a74
Sean Sullivan <dev@seanmedia.ca>
2020-07-21 12:10:28 -0600

    Modify store routes to take a database connection

--- server/routes/stores/index.js
+++ server/routes/stores/index.js
@@ -4,21 +4,23 @@ const express = require("express");
 const prizes = require("./prizes");
 const router = express.Router({ mergeParams: true });
 
-// ## /{family_id}/stores
-// ### GET Method
-// Get list of available stores.
-router.get("/", (req, res) => {
-  res.send(`GET /:family_id/stores route hit`);
-});
+module.exports = (db) => {
+  // ## /{family_id}/stores
+  // ### GET Method
+  // Get list of available stores.
+  router.get("/", (req, res) => {
+    res.send(`GET /:family_id/stores route hit`);
+  });
 
-// ## /{family_id}/stores/{store_id}
-// ### GET Method
-// Get details of store (including list of available prizes and discounts).
-router.get("/:store_id", (req, res) => {
-  res.send(`GET /:family_id/stores/:store_id route hit`);
-});
+  // ## /{family_id}/stores/{store_id}
+  // ### GET Method
+  // Get details of store (including list of available prizes and discounts).
+  router.get("/:store_id", (req, res) => {
+    res.send(`GET /:family_id/stores/:store_id route hit`);
+  });
 
-// ## /{family_id}/stores/{store_id}/prizes routes
-router.use("/:store_id/prizes", prizes);
+  // ## /{family_id}/stores/{store_id}/prizes routes
+  router.use("/:store_id/prizes", prizes(db));
 
-module.exports = router;
+  return router;
+};
