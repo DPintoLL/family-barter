0d64a9307f49ca5149aa9852752a5a6d00de993d
Sean Sullivan <dev@seanmedia.ca>
2020-07-22 11:27:15 -0600

    Add custom middleware to server/index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const PORT = process.env.PORT || 8000;
 const ENV = require("./environment");
 
-const db = require("../db");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+
+const { authenticate } = require("./middleware/authenticate");
 const setupRoutes = require("./routes");
+const db = require("../db");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["testkey1", "testkey2", "testkey3"],
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
+app.use(authenticate(db));
 setupRoutes(app, db);
 
 app.listen(PORT, () => {
