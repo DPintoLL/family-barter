1dec885a83f00f8e1398fed8ac2a68ec625f6568
kevinconvery <kevinconvery@gmail.com>
2020-07-22 22:45:07 -0400

    add routes to prizes -wip

--- server/routes/prizes/index.js
+++ server/routes/prizes/index.js
@@ -7,26 +7,35 @@ module.exports = (db) => {
   // ## /{family_id}/prizes
   // ### GET Method
   // Return list of available prizes.
-  router.get("/", (req, res) => {
-    res.send(`GET /:family_id/prizes route hit`);
+  router.get("/", async (req, res) => {
+    const { family_id } = req.params
+    const data = await db.query(`SELECT * FROM prizes WHERE family_id = $1`, [family_id])
+    res.send(data)
   });
 
   // ### POST Method
   // Create a new prize.
-  router.post("/", (req, res) => {
-    res.send(`POST /:family_id/prizes route hit`);
+  // todo: pass in the data from a form
+  router.post("/", async (req, res) => {
+    const { family_id } = req.params
+    console.log(req.body)
+    const query = `INSERT INTO prizes ()`
   });
 
   // ## /{family_id}/prizes/{prize_id}
   // ### GET Method
   // Get details of a prizes.
-  router.get("/:prize_id", (req, res) => {
-    res.send(`GET /:family_id/prizes/:prize_id route hit`);
+  router.get("/:prize_id", async (req, res) => {
+    const { prize_id } = req.params
+    const data = await(`SELECT * FROM prizes WHERE prize_id = $1`, [prize_id])
+    res.send(data)
   });
 
   // ### PUT Method
   // Update reward on server.
+  // todo: pass in the data from a form
   router.put("/:prize_id", (req, res) => {
+    const { prize_id } = 
     res.send(`PUT /:family_id/prizes/:prize_id route hit`);
   });
 
