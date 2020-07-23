29ae2630ef18d10f631c4d381b685a816f7c9e14
kevinconvery <kevinconvery@gmail.com>
2020-07-23 17:42:54 -0400

    add post route to prizes

--- server/routes/prizes/index.js
+++ server/routes/prizes/index.js
@@ -1,25 +1,39 @@
 // ./routes/prizes/index.js
-
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-
 module.exports = (db) => {
   // ## /{family_id}/prizes
   // ### GET Method
   // Return list of available prizes.
-  router.get("/", async (req, res) => {
-    const { family_id } = req.params
-    const data = await db.query(`SELECT * FROM prizes WHERE family_id = $1`, [family_id])
-    res.send(data)
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM prizes`)
+    .then(data => res.json(data))
   });
 
   // ### POST Method
   // Create a new prize.
   // todo: pass in the data from a form
-  router.post("/", async (req, res) => {
-    const { family_id } = req.params
-    console.log(req.body)
-    const query = `INSERT INTO prizes ()`
+  router.post("/", (req, res) => {
+    // const { name, description, cost, icon_url, icon_color, is_shared, created_by } = req.body
+    const queryString = `
+      INSERT INTO prizes
+        (family_id, name, description, cost, icon_url, icon_color, is_shared, created_by)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
+    `
+    const queryParams = [
+      1, 
+      'Chocolate chip cookie', 
+      'Available for a limited time only, this delicious cookie will improve mental focus and bring joy!',
+      '100',
+      'ICON_URL_HERE',
+      '#FFFFFF',
+      true,
+      1
+    ]
+    db.query(queryString, queryParams)
+    .then(() => res.send('Prize successfully created'))
+    .catch((err) => console.error(`error in creating prize: ${err}`))
+    .finally(db)
   });
 
   // ## /{family_id}/prizes/{prize_id}
@@ -27,7 +41,7 @@ module.exports = (db) => {
   // Get details of a prizes.
   router.get("/:prize_id", async (req, res) => {
     const { prize_id } = req.params
-    const data = await(`SELECT * FROM prizes WHERE prize_id = $1`, [prize_id])
+    const data = await db.query(`SELECT * FROM prizes WHERE prize_id = $1`, [prize_id])
     res.send(data)
   });
 
@@ -35,7 +49,6 @@ module.exports = (db) => {
   // Update reward on server.
   // todo: pass in the data from a form
   router.put("/:prize_id", (req, res) => {
-    const { prize_id } = 
     res.send(`PUT /:family_id/prizes/:prize_id route hit`);
   });
 
